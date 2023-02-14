082557dfdae75214b626dfbb7908007f8e19a35c
Aliaksandr Dvornik <aadvornik@gmail.com>
2023-02-14 23:06:31 +0300

    updated model.py by adding a main function

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import os
 from datetime import datetime
+import numpy as np
+from scipy import stats
 from typing import Any
 
 
@@ -15,3 +17,12 @@ def save_to_file(obj: Any, name: str):
 def save_figure(obj: Any, ):
     with open(SAMPLE_DIR + f'fig_{datetime.now().date()}.png', 'w') as outfile:
         outfile.write(obj)
+
+
+def return_base_statistics(data_: np.ndarray):
+    mean = np.mean(data_)
+    std = np.std(data_)
+    sem = stats.sem(data_)
+    min_ = min(data_)
+    max_ = max(data_)
+    return float(mean), float(sem), float(std), int(max_), int(min_)
